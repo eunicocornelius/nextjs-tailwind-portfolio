373fc58744056ff3890841c7d0c3fe4de49aacaa
Nico <eunico.cornelius@gmail.com>
2022-10-07 22:28:33 +0700

    enable spin animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,17 @@ module.exports = {
         burtons: "burtons",
       },
       animation: {
-        'blob': 'blob ease-in-out infinite 8s',
+        'blob': 'blob ease-in-out infinite 8s, spin 100s linear infinite',
       },
       keyframes: {
         blob: {
-          '0%, 100%': { borderRadius: '65% 100% 80% 100%'},
-          '25%': {borderRadius: '100% 80% 100% 65%'},
-          '50%': { borderRadius: '80% 100% 65% 100%'},
-          '75%': {borderRadius: '100% 65% 100% 80%'},
+          '0%, 100%': { borderRadius: '24% 76% 35% 65% / 27% 36% 64% 73%'},
+          '50%': {borderRadius: '76% 24% 33% 67% / 68% 55% 45% 32%'},
         },
+        spin: {
+          '0%': { transform: 'rotate(0deg)'},
+          '100%': {transform: 'rotate(359deg)'},
+        }
       }
     },
   },
